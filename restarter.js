62fad26fa58807d7c2a13066cf1c885e8e558e25
Nicholas Zavagli <nick10512@aol.com>
2018-08-03 23:56:12 -0400

    make code run for multiple miners

--- restarter.js
+++ restarter.js
@@ -1,45 +1,52 @@
 const CGMinerClient = require('cgminer-api')
 const CronJob = require('cron').CronJob
 const exec = require('child_process').exec
+require('dotenv').config()
 
 // Make a client to talk to a miner
-const cgminer = new CGMinerClient({
-  host: '192.168.128.27',
-  port: 4028
-});
+// const cgminer = new CGMinerClient({
+//   host: '192.168.128.27',
+//   port: 4028
+// });
+
+const ipaddresses = process.env.IP_ADDRESSES.split(',')
+miners = []
+for (ipaddress of ipaddresses) {
+  miners.push(new CGMinerClient({
+    host: ipaddress,
+    port: 4028
+  }))
+}
 
 // Create a job to run every 15 minutes
-const restartJob = new CronJob('0 */15 * * * *', function () {
-  restartIfNecessary()
-})
+
+function runRestartJob () {
+  console.log("Checking all miners")
+  for (miner of miners) {
+    restartIfNecessary(miner)
+  }
+}
+
+const restartJob = new CronJob('0 */15 * * * *', runRestartJob)
 
 // Start the timer
 restartJob.start()
 
-response = {
-  'SUMMARY': [
-    {
-      'GHS 5s': '10000'
-    }
-  ]
-}
-
-function restartIfNecessary () {
+function restartIfNecessary (miner) {
   // Get summary information
-  cgminer.sendCommand('summary', function (err, response) {
+  miner.sendCommand('summary', function (err, response) {
     if (err != undefined) {
       return
     }
 
-    console.log(response)
     console.log('Hashrate: ' + response['SUMMARY'][0]['GHS 5s'])
 
     // Restart if machine has 0 hashrate
-    if (response['SUMMARY'][0]['GHS 5s'] !== '0') {
+    if (response['SUMMARY'][0]['GHS 5s'] === '0') {
       console.log('Restarting')
       // cgminer.sendCommand('restart', function (err, response) {
       exec('sshpass -v -f rootpass.txt ssh -o "StrictHostKeyChecking no" root@192.168.128.24 /sbin/reboot', function (err, stdout, stderr) {
-        console.log(err, response)
+        console.log(err, stdout, stderr)
         console.log('Restart complete')
       })
     }
@@ -47,38 +54,4 @@ function restartIfNecessary () {
   })
 }
 
-// Check for restart on startup
-restartIfNecessary()
-
-const addThenDoSomething = function (x, func) {
-  const y = x + x
-  func(y)
-}
-
-const log = function (message) {
-  console.log(message)
-}
-
-addThenDoSomething(3, log)
-
-//
-// var integer = 8
-// var number = 8.3
-// var string = 'hello'
-// var boolean = true
-// var object = {
-//   int: integer,
-//   num: number,
-//   str: string,
-//   bool: boolean
-// }
-// console.log(object.num)
-//
-// var array = [1, 2, 3, 4]
-// console.log(array[0])
-//
-// if (boolean) {
-//   console.log("TRUE")
-// } else {
-//   console.log("FALSE")
-// }
+runRestartJob()
